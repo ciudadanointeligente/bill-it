59e97fd7e5103ecf093a35d0499fc51f2493fd3a
Marcel Augsburger <maugsburger@votainteligente.cl>
2013-03-28 12:50:32 -0400

    Added rails file structure

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 3.2'
+
+#Search
+#latest version of sunspot_mongoid
+#(the one in rubygems doesn't work by march 2013)
+gem 'sunspot_mongoid', :git => "git://github.com/jugyo/sunspot_mongoid.git"
+gem 'sunspot_solr'
+
+#Representers
+gem 'roar', '~> 0.11'
+gem 'roar-rails', "~> 0.0"
+gem 'billit_representers', '~> 0.0'
+
+#Test
+group :test do
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BillIt::Application.config.secret_token = '75cc973db60d3e07beaa1630c4cb37ded228e5bb71853be068a573b1a2ee385379111f9b12847b285a7e2c2b2f918b2902f4edb04046319cf41148a642fa53d3'
