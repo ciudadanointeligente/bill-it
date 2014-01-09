395fee45471dd6528f1d0228ad9ca110e76d43ec
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2014-01-09 17:52:34 -0300

    Updates production config for representers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,4 +61,9 @@ BillIt::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  # Representers
+  config.representer.default_url_options = {:host => ENV['root_url']}
+
+  # Limit log disk space
+  config.logger = Logger.new("log/#{Rails.env}.log", 1, ENV['log_size_limit'])
 end
