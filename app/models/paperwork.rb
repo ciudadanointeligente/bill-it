0a772023d87e694cbc9ad83a47b7ca769f5f29ac
Marcel Augsburger <maugsburger@gmail.com>
2014-02-25 11:13:38 -0300

    Added timeline_status field to paperworks

--- app/models/paperwork.rb
+++ app/models/paperwork.rb
@@ -5,7 +5,7 @@ class Paperwork
 
   belongs_to :bill
 
-  before_save :save_bill_uid, :index_paperwork
+  before_save :save_bill_uid, :index_paperwork, :define_timeline_status
 
   field :session, :type => String
   field :date, :type => DateTime
@@ -13,6 +13,7 @@ class Paperwork
   field :stage, :type => String
   field :chamber, :type => String
   field :bill_uid, :type => String
+  field :timeline_status, :type => String
 
   include Sunspot::Mongoid2
   searchable do
@@ -30,5 +31,32 @@ class Paperwork
 
   def index_paperwork
     Sunspot.index!(self)
+  end
+
+  def define_timeline_status
+    timeline_status_value = 'Estado por Defecto'
+    timeline_status_hash.values.each do |match_strings|
+      match_strings.each do |match_string|
+        if self.description =~ /(?imx:#{Regexp.quote(match_string)})/
+          timeline_status_value = timeline_status_hash.key match_strings
+        end
+      end
+    end
+    self.timeline_status = timeline_status_value
+  end
+
+  def timeline_status_hash
+    {
+      'Ingreso' => ['ingreso', 'iniciativa'],
+      'Avanza' => ['pasa a'],
+      'Indicaciones' => ['indicaciones', 'indicación'],
+      'Votación' => ['discusión'],
+      'Urgencia' => ['hace presente la urgencia'],
+      'Rechazado' => ['rechazado', 'rechazo'],
+      'Inasistencia' => ['insistencia'],
+      'Descartado' => ['retira', 'retirado'],
+      'Informe' => ['informe'],
+      'Retiro de Urgencia' => ['retira la urgencia']
+    }
   end
 end
