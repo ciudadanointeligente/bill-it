61b263fe356cd4642964a366fa46bb06261741cb
Marcel Augsburger <maugsburger@gmail.com>
2014-06-03 10:20:07 -0400

    Added motion factory spec

--- spec/models/motion_spec.rb
+++ spec/models/motion_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Motion do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.create(:motion).should be_valid
+  end
+
+  # it "is invalid without an uique id (uid)" do
+  #   FactoryGirl.build(:bill, uid: nil).should_not be_valid
+  # end
 end
